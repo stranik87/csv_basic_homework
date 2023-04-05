77a6f084ac6b7d4def57e3dcbdc8fcd44e502dad
stranik87 <tulkin.boynazarov@gmail.com>
2023-04-05 22:08:36 +0300

    csv_rows

--- find_number_of_rows.py
+++ find_number_of_rows.py
@@ -1,3 +1,5 @@
+import csv
+
 def find_number_of_rows(data):
     """
     Find the number of rows in CSV.
@@ -6,6 +8,16 @@ def find_number_of_rows(data):
     Return:
         int: Number of rows.
     """
-    return
-
+    cnt = 0
+    x = 0
+    lst = []
+    
+    f = open(data)
+    reader = csv.reader(f)
+    for i in reader:
+        lst.append(i[0])
+    while x < len(lst):
+        x += 1
+    return data, x
+print(find_number_of_rows("data.csv"))
 # Read the csv file

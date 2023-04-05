0bd0cab8934f98db3fcdbce6978181b6328cfce9
stranik87 <tulkin.boynazarov@gmail.com>
2023-04-05 22:27:35 +0300

    csv_row

--- get_first_row.py
+++ get_first_row.py
@@ -1,3 +1,4 @@
+import csv
 def get_first_row(data):   
    """
    Get the first row from a CSV file.
@@ -6,7 +7,13 @@ def get_first_row(data):
     Return:
         list: First row.
    """
+   f = open(data)
+   reader = csv.reader(f)
+   for i in reader:
+       print(i[1])
    
-   return
+   return data
+
+print(get_first_row("data.csv"))
 
 # Read the csv file
